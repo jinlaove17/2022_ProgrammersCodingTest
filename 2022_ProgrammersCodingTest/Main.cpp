7e172d79a731620baa0f05756cb87ffcf2eb525a
JongWoo <jinlaove17@naver.com>
2022-06-23 22:25:39 +0900

    [Lv.1] 직사각형 별찍기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main()
 {
+	int n{}, m{};
 
+	cin >> n >> m;
+
+	for (int i = 0; i < m; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+	}
 }
