44c1c20bf1ba462b1fe80df128f8ac79de4edee9
JongWoo <jinlaove17@naver.com>
2022-06-27 12:29:05 +0900

    [Lv.1] 콜라츠 추측

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,18 +1,31 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
-double solution(vector<int> arr)
+int solution(int num)
 {
-    double answer{};
-
-    for (int n : arr)
+    int answer{};
+    
+    if (num > 1)
     {
-        answer += n;
-    }
+        // 문제 오류 : int num은 홀수 조건을 수행하다가 오버플로우가 발생함.
+        long num2 = num;
 
-    answer /= arr.size();
+        while (num2 != 1 && answer < 500)
+        {
+            if (num2 % 2 == 0)
+            {
+                num2 /= 2;
+            }
+            else
+            {
+                num2 = 3 * num2 + 1;
+            }
+
+            answer += 1;
+        }
+
+        if (answer >= 500)
+        {
+            answer = -1;
+        }
+    }
 
     return answer;
 }
