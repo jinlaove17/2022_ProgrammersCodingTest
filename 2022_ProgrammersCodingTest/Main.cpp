6ff530c9107c74f7a519876abdaee66f68b6ab73
JongWoo <jinlaove17@naver.com>
2022-06-28 13:01:02 +0900

    [Lv.1] 짝수와 홀수

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,40 +1,8 @@
-#include <vector>
+#include <string>
 
 using namespace std;
 
-int CalculateGCD(int greater, int less)
+string solution(int num)
 {
-    int remainder{};
-
-    while (less != 0)
-    {
-        remainder = greater % less;
-        greater = less;
-        less = remainder;
-    }
-
-    return greater;
-}
-
-int CalculateLCM(int greater, int less)
-{
-    return greater * less / CalculateGCD(greater, less);
-}
-
-vector<int> solution(int n, int m)
-{
-    vector<int> answer{};
-
-    if (n > m)
-    {
-        answer.push_back(CalculateGCD(n, m));
-        answer.push_back(CalculateLCM(n, m));
-    }
-    else
-    {
-        answer.push_back(CalculateGCD(m, n));
-        answer.push_back(CalculateLCM(m, n));
-    }
-   
-    return answer;
+    return (num % 2 == 0) ? "Even" : "Odd";
 }
