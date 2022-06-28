25cb2a72a8133044b829b0a4334cd5c2b693183a
JongWoo <jinlaove17@naver.com>
2022-06-28 13:23:44 +0900

    [Lv.1] 제일 작은 수 제거하기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,8 +1,33 @@
-#include <string>
+#include <vector>
+#include <climits>
 
 using namespace std;
 
-string solution(int num)
+vector<int> solution(vector<int> arr)
 {
-    return (num % 2 == 0) ? "Even" : "Odd";
+    if (arr.size() == 1)
+    {
+        arr[0] = -1;
+    }
+    else
+    {
+        int arrSize{ static_cast<int>(arr.size()) };
+        int minValue{ INT_MAX };
+        int minIndex{};
+
+        for (int i = 0; i < arrSize; ++i)
+        {
+            if (arr[i] < minValue)
+            {
+                minValue = arr[i];
+                minIndex = i;
+            }
+        }
+
+        arr.erase(next(arr.begin(), minIndex));
+    }
+
+    return arr;
 }
+
+// 다음번에 풀 때는 min_element라는 함수를 공부하고 사용해보자.
