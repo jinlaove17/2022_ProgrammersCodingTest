8b3d607ac2192164ea2171845183ffb1a8599840
JongWoo <jinlaove17@naver.com>
2022-06-30 22:21:12 +0900

    [Lv.1] 시저 암호

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,12 +1,27 @@
-int solution(int n)
-{
-    int answer{};
+#include <string>
 
-    for (int i = 1; i <= n; ++i)
+using namespace std;
+
+string solution(string s, int n)
+{
+    int length{ static_cast<int>(s.length()) };
+    string answer{};
+
+    answer.reserve(length);
+
+    for (int i = 0; i < length; ++i)
     {
-        if (n % i == 0)
+        if (s[i] == ' ')
         {
-            answer += i;
+            answer.push_back(' ');
+        }
+        else if (isupper(s[i]))
+        {
+            answer.push_back(65 + (s[i] % 65 + n) % 26);
+        }
+        else if (islower(s[i]))
+        {
+            answer.push_back(97 + (s[i] % 97 + n) % 26);
         }
     }
 
