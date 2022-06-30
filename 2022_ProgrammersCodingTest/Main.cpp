82002d293fa7cb28698eec04484f89168b126948
JongWoo <jinlaove17@naver.com>
2022-06-30 22:25:23 +0900

    [Lv.1] 수박수박수박수박수박수?

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -2,26 +2,19 @@
 
 using namespace std;
 
-string solution(string s, int n)
+string solution(int n)
 {
-    int length{ static_cast<int>(s.length()) };
     string answer{};
 
-    answer.reserve(length);
-
-    for (int i = 0; i < length; ++i)
+    for (int i = 0; i < n; ++i)
     {
-        if (s[i] == ' ')
+        if (i % 2 == 0)
         {
-            answer.push_back(' ');
+            answer.append("¼ö");
         }
-        else if (isupper(s[i]))
+        else
         {
-            answer.push_back(65 + (s[i] % 65 + n) % 26);
-        }
-        else if (islower(s[i]))
-        {
-            answer.push_back(97 + (s[i] % 97 + n) % 26);
+            answer.append("¹Ú");
         }
     }
 
