2766616b34dd36d61313a146bdd4c7f091e0140a
JongWoo <jinlaove17@naver.com>
2022-07-01 23:45:24 +0900

    [Lv.1] 문자열 다루기 기본

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,30 +1,25 @@
-#include <cmath>
+#include <string>
 
-int solution(int n)
+using namespace std;
+
+bool solution(string s)
 {
-    int answer{};
+    bool answer{};
+    int length{ static_cast<int>(s.length()) };
 
-    for (int i = 2; i <= n; ++i)
+    if (length == 4 || length == 6)
     {
-        int sqrtNum = static_cast<int>(sqrt(i));
-        bool isDivided{};
+        answer = true;
 
-        for (int j = 2; j <= sqrtNum; ++j)
+        for (char c : s)
         {
-            if (i % j == 0)
+            if (!isdigit(c))
             {
-                isDivided = true;
+                answer = false;
                 break;
             }
-        }
-
-        if (!isDivided)
-        {
-            answer += 1;
         }
     }
 
     return answer;
 }
-
-// 에라토스테네스의 체 알고리즘도 한번 살펴보자.
