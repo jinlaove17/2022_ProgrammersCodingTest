f354cfeaa88e953a362d599a06e090dbfb26764d
JongWoo <jinlaove17@naver.com>
2022-07-05 23:44:10 +0900

    [Lv.1] 나누어 떨어지는 숫자 배열

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,28 +1,25 @@
-long long solution(int a, int b)
-{
-    long long answer{};
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
-    if (a > b)
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor)
+{
+    vector<int> answer{};
+
+    copy_if(arr.begin(), arr.end(), back_inserter(answer), [divisor](int n) {
+        return n % divisor == 0;
+        });
+
+    if (answer.size() == 0)
     {
-        for (int i = b; i <= a; ++i)
-        {
-            answer += i;
-        }
-    }
-    else if (a == b)
-    {
-        answer = a;
+        answer.push_back(-1);
     }
     else
     {
-        for (int i = a; i <= b; ++i)
-        {
-            answer += i;
-        }
+        sort(answer.begin(), answer.end());
     }
-
-
-
 
     return answer;
 }
