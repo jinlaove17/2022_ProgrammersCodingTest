800c94068ef04005ef773dc0a4066141ac26ddd3
JongWoo <jinlaove17@naver.com>
2022-07-13 00:17:06 +0900

    [Lv.1] 나머지가 1이 되는 수 찾기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,32 +1,22 @@
-#include <string>
-
-using namespace std;
-
-string solution(int a, int b)
+int solution(int n)
 {
-    int totalDays{ b };
-    int monthOfDays[]{ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    int answer{};
 
-    for (int i = 0; i < a - 1; ++i)
+    if (n & 1)
     {
-        totalDays += monthOfDays[i];
+        answer = 2;
+    }
+    else
+    {
+        for (int i = 3; i < n; i += 2)
+        {
+            if (n % i == 1)
+            {
+                answer = i;
+                break;
+            }
+        }
     }
 
-    switch (totalDays % 7)
-    {
-    case 0:
-        return "THU";
-    case 1:
-        return "FRI";
-    case 2:
-        return "SAT";
-    case 3:
-        return "SUN";
-    case 4:
-        return "MON";
-    case 5:
-        return "TUE";
-    case 6:
-        return "WED";
-    }
+    return answer;
 }
