a96e3bd00c2721509c91dc174c0690c36c1b3073
JongWoo <jinlaove17@naver.com>
2022-07-14 01:17:25 +0900

    [Lv.1] 부족한 금액 계산하기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,67 +1,16 @@
-#include <string>
-#include <vector>
-#include <algorithm>
+//#include <cmath>
 
-using namespace std;
-
-string solution(vector<string> participant, vector<string> completion)
+long long solution(int price, int money, int count)
 {
-    string answer{};
+    long long answer{};
+    long long totalPrice{};
 
-    sort(participant.begin(), participant.end());
-    sort(completion.begin(), completion.end());
-
-    for (int i = 0; i < participant.size(); ++i)
+    for (int i = 1; i <= count; ++i)
     {
-        if (participant[i] != completion[i])
-        {
-            answer = participant[i];
-            break;
-        }
+        totalPrice += price * i;
     }
 
-    return answer;
+    answer = money - totalPrice;
+
+    return (answer > 0) ? 0 : -answer; // abs(answer); 이건 왜 안되는지..?
 }
-
-
-//#include <string>
-//#include <vector>
-//#include <set>
-//
-//using namespace std;
-//
-//string solution(vector<string> participant, vector<string> completion)
-//{
-//    string answer{};
-//
-//    const set<string> uniqueParticipantNames{ participant.cbegin(), participant.cend() };
-//    const set<string> uniqueCompletionNames{ completion.cbegin(), completion.cend() };
-//
-//    if (uniqueParticipantNames.size() != uniqueCompletionNames.size())
-//    {
-//        for (auto iter = uniqueParticipantNames.cbegin(); iter != uniqueParticipantNames.cend(); ++iter)
-//        {
-//            if (uniqueCompletionNames.find(*iter) == uniqueCompletionNames.cend())
-//            {
-//                answer = *iter;
-//                break;
-//            }
-//        }
-//    }
-//    else
-//    {
-//         const multiset<string> participantNames{ participant.cbegin(), participant.cend() };
-//         const multiset<string> completionNames{ completion.cbegin(), completion.cend() };
-//         
-//        for (auto iter = uniqueParticipantNames.cbegin(); iter != uniqueParticipantNames.cend(); ++iter)
-//        {
-//            if (participantNames.count(*iter) != completionNames.count(*iter))
-//            {
-//                answer = *iter;
-//                break;
-//            }
-//        }
-//    }
-//
-//    return answer;
-//}
