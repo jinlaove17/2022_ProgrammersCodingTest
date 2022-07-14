b976cd52f1aab84920a6a2bde0b07b06080bc598
JongWoo <jinlaove17@naver.com>
2022-07-14 15:55:52 +0900

    [Lv.1] 소수 만들기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,18 +1,42 @@
 #include <vector>
-#include <set>
 
 using namespace std;
 
 int solution(vector<int> nums)
 {
     int answer{};
+    int numberCount{ static_cast<int>(nums.size()) };
 
-    set<int> types{ nums.begin(), nums.end() };
+    for (int i = 0; i < numberCount - 2; ++i)
+    {
+        for (int j = i + 1; j < numberCount - 1; ++j)
+        {
+            for (int k = i + 2; k < numberCount; ++k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
 
-    int typeCount{ static_cast<int>(types.size()) };
-    int selectCount{ static_cast<int>(nums.size() / 2) };
+                if (sum & 1)
+                {
+                    bool isPrimeNum{ true };
 
-    (typeCount >= selectCount) ? answer = selectCount : answer = typeCount;
+                    for (int l = 2; l < sum; ++l)
+                    {
+                        if (sum % l == 0)
+                        {
+                            isPrimeNum = false;
+                            break;
+                        }
+                    }
+
+                    if (isPrimeNum)
+                    {
+                        answer += 1;
+                    }
+                }
+            }
+        }
+
+    }
 
     return answer;
 }
