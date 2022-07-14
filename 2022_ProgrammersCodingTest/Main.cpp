c15310b095b3490ad3121ae78b275595795e7996
JongWoo <jinlaove17@naver.com>
2022-07-14 18:26:40 +0900

    [Lv.1] 예산

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -3,60 +3,23 @@
 
 using namespace std;
 
-int solution(int n, vector<int> lost, vector<int> reserve)
+int solution(vector<int> d, int budget)
 {
-    int lostCount{ static_cast<int>(lost.size()) };
-    int answer{ n - lostCount };
+    int answer{};
+    int usedBudget{};
 
-    for (int i = 0; i < lostCount; ++i)
+    sort(d.begin(), d.end());
+
+    for (int i = 0; i < d.size(); ++i)
     {
-        auto iter{ find_if(reserve.begin(), reserve.end(), [lost, i, &answer](int num) {
-            if (num == lost[i])
-            {
-                answer += 1;
-
-                return true;
-            }
-
-            return false;
-            }) };
-
-        if (iter != reserve.end())
+        if (usedBudget + d[i] <= budget)
         {
-            lost.erase(find(lost.begin(), lost.end(), lost[i]));
-            reserve.erase(iter);
-
-            --i;
+            usedBudget += d[i];
+            answer += 1;
         }
-    }
-
-    sort(lost.begin(), lost.end());
-    sort(reserve.begin(), reserve.end());
-
-    lostCount = static_cast<int>(lost.size());
-
-    for (int i = 0; i < lostCount; ++i)
-    {
-        auto iter{ find_if(reserve.begin(), reserve.end(), [lost, i, &answer](int num) {
-            if (num == lost[i] - 1)
-            {
-                answer += 1;
-
-                return true;
-            }
-            else if (num == lost[i] + 1)
-            {
-                answer += 1;
-
-                return true;
-            }
-
-            return false;
-            }) };
-
-        if (iter != reserve.end())
+        else
         {
-            reserve.erase(iter);
+            break;
         }
     }
 
