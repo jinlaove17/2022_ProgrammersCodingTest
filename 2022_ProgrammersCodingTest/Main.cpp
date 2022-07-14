0e9700505313cefcf174858ab504debe0f2c2e56
JongWoo <jinlaove17@naver.com>
2022-07-14 15:22:30 +0900

    [Lv.1] 음양 더하기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,28 +1,14 @@
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-vector<int> solution(vector<int> arr, vector<vector<int>> commands)
+int solution(vector<int> absolutes, vector<bool> signs)
 {
-    vector<int> answer{};
+    int answer{};
 
-    answer.reserve(commands.size());
-
-    for (int i = 0; i < commands.size(); ++i)
+    for (int i = 0; i < absolutes.size(); ++i)
     {
-        vector<int> dividedArr{};
-
-        dividedArr.reserve(commands[i][1] - commands[i][0] + 1);
-
-        for (int j = commands[i][0] - 1; j <= commands[i][1] - 1; ++j)
-        {
-            dividedArr.push_back(arr[j]);
-        }
-
-        sort(dividedArr.begin(), dividedArr.end());
-
-        answer.push_back(dividedArr[commands[i][2] - 1]);
+        answer += (signs[i]) ? absolutes[i] : -absolutes[i];
     }
 
     return answer;
