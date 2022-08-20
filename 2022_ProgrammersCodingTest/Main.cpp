38e4da3a5e28d6e2226b92a256ea4fc75ac2b620
JongWoo <jinlaove17@naver.com>
2022-08-20 13:43:38 +0900

    [Lv.2] H-Index

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,32 +1,25 @@
-#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-string solution(vector<int> numbers)
+int solution(vector<int> citations)
 {
-    string answer = "";
+    int answer = 0;
 
-    sort(numbers.begin(), numbers.end(),
-        [](int a, int b)
+    sort(citations.begin(), citations.end());
+
+    for (int n = citations.size(); n >= 0; --n)
+    {
+        int pivot = lower_bound(citations.begin(), citations.end(), n) - citations.begin();
+        int quotedCount = (citations.size() - 1) - pivot + 1;
+        int restCount = pivot - 1;
+
+        if (quotedCount >= n && restCount <= n)
         {
-            string s1 = to_string(a) + to_string(b);
-            string s2 = to_string(b) + to_string(a);
-
-            return s1 > s2;
-        });
-
-    if (numbers[0] == 0)
-    {
-        answer = "0";
-
-        return answer;
-    }
-
-    for (int number : numbers)
-    {
-        answer += to_string(number);
+            answer = n;
+            break;
+        }
     }
 
     return answer;
