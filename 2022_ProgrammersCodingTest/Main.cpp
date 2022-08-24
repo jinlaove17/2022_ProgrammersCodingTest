d4f6a46e9a1f37e99c1d0b0c8952454d01aa6ef7
JongWoo <jinlaove17@naver.com>
2022-08-24 23:53:39 +0900

    [Lv.3] 가장 긴 팰린드롬

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,53 +1,47 @@
-#include <unordered_set>
+#include <string>
+#include <array>
 
 using namespace std;
 
-int solution(int N, int number)
+array<array<bool, 2500>, 2500> DP;
+
+int solution(string s)
 {
-    int answer = -1;
-    unordered_set<int> us[8];
-    int base = 0;
+    // 알파벳 하나는 팰린드롬이므로, 최소 길이는 1이다.
+    int answer = 1;
 
-    for (int i = 0; i < 8; ++i)
+    // 길이가 1 또는 2인 문자열은 팰린드롬이므로, DP의 값을 갱신한다.
+    for (int i = 0; i < s.length(); ++i)
     {
-        base = 10 * base + N;
+        DP[i][i] = true;
 
-        us[i].insert(base);
-    }
-
-    if (us[0].count(number) > 0)
-    {
-        answer = 1;
-
-        return answer;
-    }
-
-    for (int i = 1; i < 8; ++i)
-    {
-        for (int j = 0; j < i; ++j)
+        if (i < s.length() - 1)
         {
-            for (const auto& op1 : us[j])
+            if (s[i] == s[i + 1])
             {
-                for (const auto& op2 : us[i - j - 1])
-                {
-                    us[i].insert(op1 + op2);
-                    us[i].insert(op1 - op2);
-                    us[i].insert(op1 * op2);
-
-                    if (op2 != 0)
-                    {
-                        us[i].insert(op1 / op2);
-                    }
-                }
+                DP[i][i + 1] = true;
+                answer = 2;
             }
         }
+    }
 
-        if (us[i].count(number) > 0)
+    // 길이가 3이상인 문자열이 팰린드롬인지 검사하고, DP의 값을 갱신한다.
+    // 즉, 양 끝(start, end)의 값이 같아야 하고, 그 사이(start + 1, end - 1)가 팰린드롬이면 갱신한다.
+    for (int len = 3; len <= s.length(); ++len)
+    {
+        for (int start = 0; start <= s.length() - len; ++start)
         {
-            answer = i + 1;
-            break;
+            int end = start + len - 1;
+
+            if (s[start] == s[end] && DP[start + 1][end - 1])
+            {
+                DP[start][end] = true;
+                answer = len;
+            }
         }
     }
 
     return answer;
 }
+
+// 참고: https://yabmoons.tistory.com/545?category=946153
