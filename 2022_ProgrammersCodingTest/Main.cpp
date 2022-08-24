2af2fd788b576278fa2d8cdf1cc30b22152d58b6
JongWoo <jinlaove17@naver.com>
2022-08-24 23:55:55 +0900

    [Lv.3] 여행경로

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,47 +1,43 @@
 #include <string>
-#include <array>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
 
 using namespace std;
 
-array<array<bool, 2500>, 2500> DP;
-
-int solution(string s)
+vector<string> solution(vector<vector<string>> tickets)
 {
-    // 알파벳 하나는 팰린드롬이므로, 최소 길이는 1이다.
-    int answer = 1;
+    vector<string> answer;
+    unordered_map<string, list<string>> um;
 
-    // 길이가 1 또는 2인 문자열은 팰린드롬이므로, DP의 값을 갱신한다.
-    for (int i = 0; i < s.length(); ++i)
+    sort(tickets.begin(), tickets.end(), greater<vector<string>>());
+
+    for (const auto& ticket : tickets)
     {
-        DP[i][i] = true;
+        um[ticket[0]].push_back(ticket[1]);
+    }
 
-        if (i < s.length() - 1)
+    stack<string> s({ "ICN" });
+
+    while (!s.empty())
+    {
+        string current = s.top();
+
+        if (um.count(current) == 0 || um[current].empty())
         {
-            if (s[i] == s[i + 1])
-            {
-                DP[i][i + 1] = true;
-                answer = 2;
-            }
+            s.pop();
+            answer.push_back(current);
+        }
+        else
+        {
+            s.push(um[current].back());
+            um[current].pop_back();
         }
     }
 
-    // 길이가 3이상인 문자열이 팰린드롬인지 검사하고, DP의 값을 갱신한다.
-    // 즉, 양 끝(start, end)의 값이 같아야 하고, 그 사이(start + 1, end - 1)가 팰린드롬이면 갱신한다.
-    for (int len = 3; len <= s.length(); ++len)
-    {
-        for (int start = 0; start <= s.length() - len; ++start)
-        {
-            int end = start + len - 1;
-
-            if (s[start] == s[end] && DP[start + 1][end - 1])
-            {
-                DP[start][end] = true;
-                answer = len;
-            }
-        }
-    }
+    // 현재 answer에는 역순으로 여행경로가 저장되어 있기 때문에, 벡터를 뒤집는다.
+    reverse(answer.begin(), answer.end());
 
     return answer;
 }
-
-// 참고: https://yabmoons.tistory.com/545?category=946153
