3f8e3da87a0bef942690e7c807eae950f279e992
JongWoo <jinlaove17@naver.com>
2022-09-19 22:55:15 +0900

    [Lv.2] 최솟값 만들기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,53 +1,19 @@
-#include <string>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-string solution(string s)
+int solution(vector<int> A, vector<int> B)
 {
-    string answer = "";
-    string str = "";
+    int answer = 0;
+    int size = A.size();
 
-    int space = 0;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<int>());
 
-    for (int i = 0; i < s.length(); ++i)
+    for (int i = 0; i < size; ++i)
     {
-        if (s[i] != ' ')
-        {
-            str.push_back(s[i]);
-        }
-        else
-        {
-            ++space;
-
-            if (i < s.length() - 1 && s[i + 1] != ' ')
-            {
-                transform(str.begin(), str.end(), str.begin(), ::tolower);
-                str[0] = toupper(str[0]);
-
-                answer += str;
-
-                for (int i = 0; i < space; ++i)
-                {
-                    answer.push_back(' ');
-                }
-
-                str.clear();
-                space = 0;
-            }
-        }
-    }
-
-    if (!str.empty())
-    {
-        transform(str.begin(), str.end(), str.begin(), ::tolower);
-        str[0] = toupper(str[0]);
-        answer += str;
-
-        for (int i = 0; i < space; ++i)
-        {
-            answer.push_back(' ');
-        }
+        answer += A[i] * B[i];
     }
 
     return answer;
