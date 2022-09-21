8aaf310f1af29b795e34d95c1537cad454e9df59
JongWoo <jinlaove17@naver.com>
2022-09-21 18:48:00 +0900

    [Lv.2] 짝지어 제거하기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,28 +1,28 @@
 #include <string>
-#include <vector>
-#include <unordered_map>
+#include <stack>
 
 using namespace std;
 
-vector<int> solution(int n, vector<string> words)
+int solution(string s)
 {
-    vector<int> answer(2, 0);
-    unordered_map<string, int> um;
+    int answer = 1;
+    stack<char> st;
 
-    ++um[words[0]];
-
-    for (int i = 1; i < words.size(); ++i)
+    for (char c : s)
     {
-        if (um.count(words[i]) == 0 && (words[i - 1].back() == words[i].front()))
+        if (!st.empty() && st.top() == c)
         {
-            ++um[words[i]];
+            st.pop();
         }
         else
         {
-            answer[0] = um.size() % n + 1;
-            answer[1] = um.size() / n + 1;
-            break;
+            st.push(c);
         }
+    }
+
+    if (!st.empty())
+    {
+        answer = 0;
     }
 
     return answer;
