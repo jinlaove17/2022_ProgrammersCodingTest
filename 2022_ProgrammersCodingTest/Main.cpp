788f317001e78d647645182dc966be125aac96de
JongWoo <jinlaove17@naver.com>
2022-09-21 18:23:18 +0900

    [Lv.2] 영어 끝말잇기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,27 +1,27 @@
+#include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-vector<int> solution(int brown, int yellow)
+vector<int> solution(int n, vector<string> words)
 {
     vector<int> answer(2, 0);
+    unordered_map<string, int> um;
 
-    // 이차 연립방정식
-    // (1) 2w + 2h - 4 = brown
-    // (2) w * h = brown + yellow
-    // (1)에서 h = brown / 2 - w + 2
-    // 따라서, w * (brown / 2 - w + 2) = brown + yellow
-    int size = brown + yellow;
+    ++um[words[0]];
 
-    // brown은 최소 8개 이상이기 때문에 가로가 최소 3이상, 최대 2500이하이다.
-    for (int w = 3; w <= 2500; ++w)
+    for (int i = 1; i < words.size(); ++i)
     {
-        int h = (brown / 2 - w + 2);
-
-        if (w * h == size)
+        if (um.count(words[i]) == 0 && (words[i - 1].back() == words[i].front()))
         {
-            answer[0] = w;
-            answer[1] = h;
+            ++um[words[i]];
+        }
+        else
+        {
+            answer[0] = um.size() % n + 1;
+            answer[1] = um.size() / n + 1;
+            break;
         }
     }
 
