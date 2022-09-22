e52e438c1ad6aab46756ee16b7bace29da0c60ef
JongWoo <jinlaove17@naver.com>
2022-09-23 02:11:21 +0900

    [Lv.2] 점프와 순간 이동

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,22 +1,18 @@
-long long solution(int n)
+int solution(int n)
 {
-    long long answer = 0;
+    int answer = 0;
 
-    if (n <= 2)
+    while (n > 0)
     {
-        answer = n;
-    }
-    else
-    {
-        int dp[2001] = { 0, 1, 2, };
-
-        for (int i = 3; i <= n; ++i)
+        if (n & 1)
         {
-            // 오버플로우가 발생할 수 있으므로, 미리 나머지 연산을 수행한다.
-            dp[i] = dp[i - 1] % 1234567 + dp[i - 2] % 1234567;
+            --n;
+            ++answer;
         }
-
-        answer = dp[n] % 1234567;
+        else
+        {
+            n /= 2;
+        }
     }
 
     return answer;
