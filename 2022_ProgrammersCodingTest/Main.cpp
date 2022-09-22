d6700d51bc775608d7aad81e8e71c0c238a7b7aa
JongWoo <jinlaove17@naver.com>
2022-09-22 23:11:40 +0900

    [Lv.2] 멀리 뛰기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,16 +1,22 @@
-int solution(int n, int a, int b)
+long long solution(int n)
 {
-    int answer = 1;
-    int groupA = (a - 1) / 2 + 1;
-    int groupB = (b - 1) / 2 + 1;
+    long long answer = 0;
 
-    while (n > 0 && groupA != groupB)
+    if (n <= 2)
     {
-        groupA = (groupA - 1) / 2 + 1;
-        groupB = (groupB - 1) / 2 + 1;
-        n /= 2;
+        answer = n;
+    }
+    else
+    {
+        int dp[2001] = { 0, 1, 2, };
 
-        ++answer;
+        for (int i = 3; i <= n; ++i)
+        {
+            // 오버플로우가 발생할 수 있으므로, 미리 나머지 연산을 수행한다.
+            dp[i] = dp[i - 1] % 1234567 + dp[i - 2] % 1234567;
+        }
+
+        answer = dp[n] % 1234567;
     }
 
     return answer;
