0910f4634df349efa0f61f21b15d699ffe14a646
JongWoo <jinlaove17@naver.com>
2022-09-23 15:24:19 +0900

    [Lv.2] [1차] 캐시

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,17 +1,46 @@
-int solution(int n)
+#include <string>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities)
 {
     int answer = 0;
+    deque<string> cache;
 
-    while (n > 0)
+    for (int i = 0; i < cities.size(); ++i)
     {
-        if (n & 1)
+        auto iter = find_if(cache.begin(), cache.end(), [&cities, i](const auto& s)
+            {
+                if (_stricmp(s.c_str(), cities[i].c_str()) == 0)
+                {
+                    return true;
+                }
+
+                return false;
+            });
+
+        if (iter != cache.end())
         {
-            --n;
             ++answer;
+
+            cache.erase(iter);
+            cache.push_back(cities[i]);
         }
         else
         {
-            n /= 2;
+            answer += 5;
+
+            if (cacheSize > 0)
+            {
+                cache.push_back(cities[i]);
+
+                if (cache.size() > cacheSize)
+                {
+                    cache.pop_front();
+                }
+            }
         }
     }
 
