b4f23b3f407583b515338c7199fa5a8b2a7f1d6a
JongWoo <jinlaove17@naver.com>
2022-09-27 13:01:34 +0900

    [Lv.2] k진수에서 소수 개수 구하기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,71 +1,88 @@
 #include <string>
-#include <unordered_map>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int solution(string str1, string str2)
+string ToBinary(int n, int k);
+bool IsPrimeNumber(long long n);
+
+int solution(int n, int k)
 {
     int answer = 0;
 
-    transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
-    transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+    string s = ToBinary(n, k);
 
-    unordered_map<string, int> str1Group;
-    int len = str1.length();
+    vector<long long> v;
+    string str;
 
-    // str1에 속한 문자들을 순회하며, 두 글자로 된 다중집합을 만든다.
-    for (int i = 0; i < len - 1; ++i)
+    for (char c : s)
     {
-        if (isalpha(str1[i]) && isalpha(str1[i + 1]))
+        switch (c)
         {
-            ++str1Group[str1.substr(i, 2)];
+        case '0':
+            if (!str.empty())
+            {
+                v.push_back(atoll(str.c_str()));
+            }
+
+            str.clear();
+            break;
+        default:
+            str.push_back(c);
+            break;
         }
     }
 
-    unordered_map<string, int> str2Group;
-
-    len = str2.length();
-
-    // str2에 속한 문자들을 순회하며, 두 글자로 된 다중집합을 만든다.
-    for (int i = 0; i < len - 1; ++i)
+    if (!str.empty())
     {
-        if (isalpha(str2[i]) && isalpha(str2[i + 1]))
-        {
-            ++str2Group[str2.substr(i, 2)];
-        }
+        v.push_back(atoll(str.c_str()));
     }
 
-    int unionCount = 0;
-    int intersectionCount = 0;
-
-    // str1 다중집합을 순회하며, 합집합 수, 교집합 수를 계산한다.
-    for (const auto& p : str1Group)
+    for (long long num : v)
     {
-        int count = str2Group.count(p.first);
-
-        if (count > 0)
+        if (IsPrimeNumber(num))
         {
-            intersectionCount += min(p.second, str2Group[p.first]);
-            unionCount += max(p.second, str2Group[p.first]);
-
-            str2Group.erase(p.first);
-        }
-        else
-        {
-            unionCount += p.second;
+            ++answer;
         }
     }
-
-    // str2 다중집합을 순회하며, 나머지 원소들을 합집합 수에 추가한다.
-    for (const auto& p : str2Group)
-    {
-        unionCount += p.second;
-    }
-
-    float analogousMap = (unionCount == 0) ? 1 : static_cast<float>(intersectionCount) / unionCount;
-
-    answer = 65536 * analogousMap;
 
     return answer;
+}
+
+string ToBinary(int n, int k)
+{
+    string s;
+
+    while (n > 0)
+    {
+        s = string(1, n % k + '0') + s;
+        n /= k;
+    }
+
+    return s;
+}
+
+bool IsPrimeNumber(long long n)
+{
+    bool isPrimeNumber = true;
+
+    if (n <= 1)
+    {
+        isPrimeNumber = false;
+    }
+    else
+    {
+        long long sqrtN = sqrtf(n);
+
+        for (long long i = 2; i <= sqrtN; ++i)
+        {
+            if (n % i == 0)
+            {
+                isPrimeNumber = false;
+                break;
+            }
+        }
+    }
+
+    return isPrimeNumber;
 }
