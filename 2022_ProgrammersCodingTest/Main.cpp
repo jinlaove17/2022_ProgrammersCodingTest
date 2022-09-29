ed5d151c4d54e13eb3de88c69679b770c92361bf
JongWoo <jinlaove17@naver.com>
2022-09-29 18:25:52 +0900

    [Lv.2] [3차] n진수 게임

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,55 +1,46 @@
 #include <string>
-#include <vector>
-#include <unordered_map>
 
 using namespace std;
 
-vector<string> solution(vector<string> record)
+string ToBinary(int n, int num);
+
+string solution(int n, int t, int m, int p)
 {
-    vector<string> answer;
+    string answer = "";
 
-    unordered_map<string, string> um;
-    int size = record.size();
+    string s = "01";
+    int num = 2;
 
-    answer.reserve(size);
-
-    for (const string& s : record)
+    while (s.length() < m * t)
     {
-        string tmp(s);
-
-        const char* state = strtok(const_cast<char*>(tmp.c_str()), " ");
-        const char* uid = strtok(NULL, " ");
-        const char* name = strtok(NULL, " ");
-
-        // 마지막 토큰인 name에 대하여 NULL체크를 안하면 값이 이상하다.(이유?)
-        if (name != NULL)
-        {
-            um[uid] = name;
-        }
-
-        if (strcmp(state, "Enter") == 0)
-        {
-            answer.emplace_back("님이 들어왔습니다.");
-        }
-        else if (strcmp(state, "Leave") == 0)
-        {
-            answer.emplace_back("님이 나갔습니다.");
-        }
+        s += ToBinary(n, num);
+        ++num;
     }
 
-    for (int i = 0, j = 0; i < size; ++i)
+    for (int i = p - 1; answer.size() < t; i += m)
     {
-        string tmp(record[i]);
-
-        const char* state = strtok(const_cast<char*>(tmp.c_str()), " ");
-        const char* uid = strtok(NULL, " ");
-
-        if (strcmp(state, "Enter") == 0 || strcmp(state, "Leave") == 0)
-        {
-            answer[j] = um[uid] + answer[j];
-            ++j;
-        }
+        answer.push_back(s[i]);
     }
 
     return answer;
+}
+
+string ToBinary(int n, int num)
+{
+    string s;
+
+    while (num > 0)
+    {
+        char c = num % n + '0';
+
+        if (c > '9')
+        {
+            c = 'A' + c - ('9' + 1);
+        }
+
+        s = string(1, c) + s;
+        num /= n;
+    }
+
+    return s;
 }
