7b6a0bcadf8a14224a00a82d54c8240d5a1c7774
JongWoo <jinlaove17@naver.com>
2022-10-04 23:46:28 +0900

    [Lv.2] 방문 길이

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -3,49 +3,83 @@
 
 using namespace std;
 
-int solution(string skill, vector<string> skill_trees)
+struct USED_DIR
+{
+    bool up;
+    bool down;
+    bool left;
+    bool right;
+};
+
+int solution(string dirs)
 {
     int answer = 0;
-    vector<string> v;
 
-    v.reserve(skill_trees.size());
+    int size = 5 - (-5) + 1;
+    vector<vector<USED_DIR>> usedDirs(size);
 
-    for (const auto& skill_tree : skill_trees)
+    for (int i = 0; i < size; ++i)
     {
-        string s;
-
-        for (char c1 : skill_tree)
-        {
-            for (char c2 : skill)
-            {
-                if (c1 == c2)
-                {
-                    s.push_back(c1);
-                    break;
-                }
-            }
-        }
-
-        v.push_back(s);
+        usedDirs[i].resize(size);
     }
 
-    for (const string& s : v)
+    int x = 5;
+    int y = 5;
+
+    for (char dir : dirs)
     {
-        bool isValid = true;
-        int len = s.length();
-
-        for (int i = 0; i < len; ++i)
+        // 테스트 케이스 8번 이후로 다틀림 ?
+        // => 지나간 길에는 방향성이 존재하지 않음!
+        switch (dir)
         {
-            if (s[i] != skill[i])
+        case 'U':
+            if (y - 1 >= 0)
             {
-                isValid = false;
-                break;
-            }
-        }
+                if (!usedDirs[y][x].up)
+                {
+                    usedDirs[y][x].up = usedDirs[y - 1][x].down = true;
+                    ++answer;
+                }
 
-        if (isValid)
-        {
-            ++answer;
+                --y;
+            }
+            break;
+        case 'D':
+            if (y + 1 < size)
+            {
+                if (!usedDirs[y][x].down)
+                {
+                    usedDirs[y][x].down = usedDirs[y + 1][x].up = true;
+                    ++answer;
+                }
+
+                ++y;
+            }
+            break;
+        case 'L':
+            if (x - 1 >= 0)
+            {
+                if (!usedDirs[y][x].left)
+                {
+                    usedDirs[y][x].left = usedDirs[y][x - 1].right = true;
+                    ++answer;
+                }
+
+                --x;
+            }
+            break;
+        case 'R':
+            if (x + 1 < size)
+            {
+                if (!usedDirs[y][x].right)
+                {
+                    usedDirs[y][x].right = usedDirs[y][x + 1].left = true;
+                    ++answer;
+                }
+
+                ++x;
+            }
+            break;
         }
     }
 
