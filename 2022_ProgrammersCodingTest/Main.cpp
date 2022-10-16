19f77ed15039f06931a31027bc32cfdbf3a0f583
JongWoo <jinlaove17@naver.com>
2022-10-16 13:33:52 +0900

    [Lv.2] 연속 부분 수열 합의 개수

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,58 +1,33 @@
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
-vector<int> solution(int n)
+int solution(vector<int> elements)
 {
-    vector<int> answer;
+    int answer = 0;
+    unordered_set<int> s;
+    int size = elements.size();
 
-    vector<vector<int>> v(n, vector<int>(n));
-
-    int dir = 0;
-    int x = 0;
-    int y = 0;
-    int num = 1;
-
-    for (int i = 0; i < n; ++i)
+    // 길이
+    for (int i = 1; i <= size; ++i)
     {
-        switch (dir)
+        // 현재 인덱스
+        for (int j = 0; j < size; ++j)
         {
-        case 0: // Down
-            for (int j = i; j < n; ++j)
-            {
-                v[y++][x] = num++;
-            }
-            --y;
-            ++x;
-            break;
-        case 1: // Right
-            for (int j = i; j < n; ++j)
-            {
-                v[y][x++] = num++;
-            }
-            --y;
-            x -= 2;
-            break;
-        case 2: // Left Up
-            for (int j = i; j < n; ++j)
-            {
-                v[y--][x--] = num++;
-            }
-            y += 2;
-            ++x;
-            break;
-        }
+            int sum = 0;
 
-        dir = (dir + 1) % 3;
-    }
+            // 현재 인덱스부터 길이만큼의 합
+            for (int k = 0; k < i; ++k)
+            {
+                sum += elements[(j + k) % size];
+            }
 
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < i + 1; ++j)
-        {
-            answer.push_back(v[i][j]);
+            s.insert(sum);
         }
     }
+
+    answer = s.size();
 
     return answer;
 }
