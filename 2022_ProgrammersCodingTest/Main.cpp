63d18df87822db071cfa20ad2292dd8399137c50
JongWoo <jinlaove17@naver.com>
2022-10-22 01:55:18 +0900

    [Lv.2] 전력망을 둘로 나누기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -2,86 +2,74 @@
 
 using namespace std;
 
-int FindMinNode(int N, const vector<int>& distance, const vector<bool>& isVisited);
+int DFS(int n, int current, const vector<vector<bool>>& adjMat, vector<bool>& isVisited);
 
-int solution(int N, vector<vector<int>> road, int K)
+int solution(int n, vector<vector<int>> wires)
 {
-    // 1번 마을은 무조건 배달이 가능하다.
-    int answer = 1;
+    int answer = INT_MAX;
 
-    // 각 마을 간의 최소 시간(가중치)을 계산한다.
-    vector<vector<int>> g(N, vector<int>(N, INT_MAX));
+    vector<vector<bool>> adjMat(n + 1, vector<bool>(n + 1));
 
-    for (int i = 0; i < road.size(); ++i)
+    for (int i = 0; i < wires.size(); ++i)
     {
-        if (g[road[i][0] - 1][road[i][1] - 1] > road[i][2])
-        {
-            // 양방향이기 때문에 반대도 설정해준다.
-            g[road[i][0] - 1][road[i][1] - 1] = road[i][2];
-            g[road[i][1] - 1][road[i][0] - 1] = road[i][2];
-        }
+        int u = wires[i][0];
+        int v = wires[i][1];
+
+        adjMat[u][v] = adjMat[v][u] = true;
     }
 
-    // 다익스트라 알고리즘
-    vector<int> distance(N);
-    vector<bool> isVisited(N);
-    int start = 0;
-
-    for (int i = 0; i < N; ++i)
+    for (int i = 0; i < wires.size(); ++i)
     {
-        distance[i] = g[start][i];
-    }
+        int u = wires[i][0];
+        int v = wires[i][1];
 
-    isVisited[start] = true;
+        adjMat[u][v] = adjMat[v][u] = false;
 
-    for (int i = 0; i < N - 1; ++i)
-    {
-        int node = FindMinNode(N, distance, isVisited);
+        vector<bool> isVisited(n + 1);
+        vector<int> groupCount;
 
-        if (node == -1)
+        // 하나의 전선을 끊었기 때문에 무조건 2그룹으로 나뉘게 된다.
+        groupCount.reserve(2);
+
+        for (int i = 1; i <= n; ++i)
         {
-            break;
-        }
+            int count = DFS(n, i, adjMat, isVisited);
 
-        isVisited[node] = true;
-
-        for (int j = 0; j < N; ++j)
-        {
-            // 인접한 노드의 거리를 갱신한다.
-            if (!isVisited[j] && g[node][j] != INT_MAX)
+            if (count)
             {
-                if (distance[j] > distance[node] + g[node][j])
-                {
-                    distance[j] = distance[node] + g[node][j];
-                }
+                groupCount.push_back(count);
             }
         }
-    }
 
-    for (int i = 1; i < N; ++i)
-    {
-        if (distance[i] <= K)
+        answer = min(answer, abs(groupCount[0] - groupCount[1]));
+        adjMat[u][v] = adjMat[v][u] = true;
+
+        if (answer == 0)
         {
-            ++answer;
+            break;
         }
     }
 
     return answer;
 }
 
-int FindMinNode(int N, const vector<int>& distance, const vector<bool>& isVisited)
+int DFS(int n, int current, const vector<vector<bool>>& adjMat, vector<bool>& isVisited)
 {
-    int minNode = -1;
-    int minValue = INT_MAX;
+    int count = 0;
 
-    for (int i = 0; i < N; ++i)
+    if (!isVisited[current])
     {
-        if (!isVisited[i] && distance[i] < minValue)
+        isVisited[current] = true;
+        count = 1;
+
+        for (int i = 1; i <= n; ++i)
         {
-            minNode = i;
-            minValue = distance[i];
+            if (!isVisited[i] && adjMat[current][i])
+            {
+                count += DFS(n, i, adjMat, isVisited);
+            }
         }
     }
 
-    return minNode;
+    return count;
 }
