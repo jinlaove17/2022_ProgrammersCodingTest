3ff29aae2e8f4786e9e88e96549a4f5486c1ad33
JongWoo <jinlaove17@naver.com>
2023-04-17 23:32:34 +0900

    [Lv.1] 숫자 문자열과 영단어

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,83 +1,24 @@
 #include <string>
-#include <vector>
 
 using namespace std;
 
-string ToBinary(int n);
-
-vector<string> solution(int n, vector<int> arr1, vector<int> arr2)
+int solution(string s)
 {
-    vector<string> answer;
+    int answer = 0;
 
-    for (int i = 0; i < n; ++i)
+    string numbers[10] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+    for (int i = 0; i < 10; ++i)
     {
-        string map;
-        string s1 = ToBinary(arr1[i]);
-        string s2 = ToBinary(arr2[i]);
+        int pos = 0;
 
-        while (!s1.empty() && !s2.empty())
+        while ((pos = s.find(numbers[i], pos)) != string::npos)
         {
-            if (s1.back() == '1' || s2.back() == '1')
-            {
-                map = '#' + map;
-            }
-            else
-            {
-                map = ' ' + map;
-            }
-
-            s1.pop_back();
-            s2.pop_back();
+            s.replace(pos, numbers[i].length(), to_string(i));
         }
-
-        while (!s1.empty())
-        {
-            if (s1.back() == '1')
-            {
-                map = '#' + map;
-            }
-            else
-            {
-                map = ' ' + map;
-            }
-
-            s1.pop_back();
-        }
-
-        while (!s2.empty())
-        {
-            if (s2.back() == '1')
-            {
-                map = '#' + map;
-            }
-            else
-            {
-                map = ' ' + map;
-            }
-
-            s2.pop_back();
-        }
-
-        while (map.length() < n)
-        {
-            map = ' ' + map;
-        }
-
-        answer.push_back(map);
     }
+
+    answer = stoi(s);
 
     return answer;
-}
-
-string ToBinary(int n)
-{
-    string s;
-
-    while (n > 0)
-    {
-        s = to_string(n % 2) + s;
-        n /= 2;
-    }
-
-    return s;
 }
