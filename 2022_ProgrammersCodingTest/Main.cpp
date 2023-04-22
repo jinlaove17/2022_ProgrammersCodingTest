db6c5016f31f4f78506fa0da2314fb3e8de81009
JongWoo <jinlaove17@naver.com>
2023-04-22 23:53:10 +0900

    [Lv.1] 푸드 파이트 대회

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,62 +1,19 @@
+#include <string>
 #include <vector>
-#include <climits>
 
 using namespace std;
 
-int totals[1'000'000];
-
-vector<int> solution(vector<int> sequence, int k)
+string solution(vector<int> food)
 {
-    vector<int> answer(2);
+    string answer = "0";
 
-    if (sequence[0] == k)
+    for (int i = food.size() - 1; i >= 0; --i)
     {
-        return answer;
-    }
+        int cnt = food[i] / 2;
 
-    // sequence 벡터를 순회하며 연속된 부분수열의 누적합을 계산한다.
-    totals[0] = sequence[0];
-
-    for (int i = 1; i < sequence.size(); ++i)
-    {
-        if (sequence[i] == k)
+        for (int j = 0; j < cnt; ++j)
         {
-            answer[0] = answer[1] = i;
-
-            return answer;
-        }
-
-        totals[i] = totals[i - 1] + sequence[i];
-    }
-
-    // 투 포인터를 활용하여 begin의 위치에서부터 값을 비교해나간다.
-    int begin = 0, end = 1;
-    int minLen = INT_MAX;
-
-    while ((begin < end) && (end < sequence.size()))
-    {
-        int total = totals[end] - totals[begin] + sequence[begin];
-
-        if (total == k)
-        {
-            int len = end - begin + 1;
-
-            if (len < minLen)
-            {
-                minLen = len;
-                answer[0] = begin;
-                answer[1] = end;
-            }
-
-            ++begin;
-        }
-        else if (total < k)
-        {
-            ++end;
-        }
-        else
-        {
-            ++begin;
+            answer = static_cast<char>(i + '0') + answer + static_cast<char>(i + '0');
         }
     }
 
