c6c9d3156771030f485f64efcc0eaea9fbae8dbd
JongWoo <jinlaove17@naver.com>
2023-04-24 01:29:06 +0900

    [Lv.2] 수식 최대화

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,26 +1,76 @@
-#include <vector>
 #include <string>
-#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-vector<int> solution(string s)
+char prioritys[6][3] =
 {
-    vector<int> answer;
-    unordered_map<char, int> um;
+    { '+', '-', '*' },
+    { '+', '*', '-' },
+    { '-', '*', '+' },
+    { '-', '+', '*' },
+    { '*', '+', '-' },
+    { '*', '-', '+' }
+};
 
-    for (int i = 0; i < s.length(); ++i)
+long long solution(string expression)
+{
+    long long answer = 0;
+    vector<long long> operands;
+    vector<char> operators;
+    string str;
+
+    for (char c : expression)
     {
-        if (um.find(s[i]) == um.end())
+        if (('0' <= c) && (c <= '9'))
         {
-            um[s[i]] = i;
-            answer.push_back(-1);
+            str.push_back(c);
         }
         else
         {
-            answer.push_back(i - um[s[i]]);
-            um[s[i]] = i;
+            operators.push_back(c);
+            operands.push_back(stoll(str));
+            str.clear();
         }
+    }
+
+    operands.push_back(stoll(str));
+
+    for (int i = 0; i < 6; ++i)
+    {
+        vector<long long> tempOperands(operands);
+        vector<char> tempOperators(operators);
+
+        for (int j = 0; j < 3; ++j)
+        {
+            for (int k = 0; k < tempOperators.size(); )
+            {
+                if (tempOperators[k] == prioritys[i][j])
+                {
+                    switch (tempOperators[k])
+                    {
+                    case '+':
+                        tempOperands[k] = tempOperands[k] + tempOperands[k + 1];
+                        break;
+                    case '-':
+                        tempOperands[k] = tempOperands[k] - tempOperands[k + 1];
+                        break;
+                    case '*':
+                        tempOperands[k] = tempOperands[k] * tempOperands[k + 1];
+                        break;
+                    }
+
+                    tempOperands.erase(tempOperands.begin() + k + 1);
+                    tempOperators.erase(tempOperators.begin() + k);
+                }
+                else
+                {
+                    ++k;
+                }
+            }
+        }
+
+        answer = max(answer, abs(tempOperands[0]));
     }
 
     return answer;
