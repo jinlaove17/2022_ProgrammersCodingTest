465c3299a41d31dabd6d597a696c4275b79b950a
JongWoo <jinlaove17@naver.com>
2023-04-25 22:45:39 +0900

    [Lv.2] 무인도 여행

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,28 +1,66 @@
 #include <string>
 #include <vector>
+#include <queue>
+#include <utility>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo)
+vector<int> solution(vector<string> maps)
 {
     vector<int> answer;
+    int dirX[] = { 0, 0, -1, 1 };
+    int dirY[] = { 1, -1, 0, 0 };
 
-    for (int i = 0; i < photo.size(); ++i)
+    for (int y = 0; y < maps.size(); ++y)
     {
-        int score = 0;
-
-        for (const auto& str : photo[i])
+        for (int x = 0; x < maps[y].length(); ++x)
         {
-            auto iter = find(name.begin(), name.end(), str);
-
-            if (iter != name.end())
+            if (('0' <= maps[y][x]) && (maps[y][x] <= '9'))
             {
-                score += yearning[iter - name.begin()];
+                int total = 0;
+                queue<pair<int, int>> q;
+
+                q.push({ x, y });
+                total = maps[y][x] - '0';
+                maps[y][x] = 'X';
+
+                while (!q.empty())
+                {
+                    pair<int, int> current = q.front();
+                    int cx = current.first;
+                    int cy = current.second;
+
+                    q.pop();
+
+                    for (int i = 0; i < 4; ++i)
+                    {
+                        int nx = cx + dirX[i];
+                        int ny = cy + dirY[i];
+
+                        if ((0 <= ny) && (ny < maps.size()) &&
+                            (0 <= nx) && (nx < maps[ny].length()) &&
+                            ('0' <= maps[ny][nx]) && (maps[ny][nx] <= '9'))
+                        {
+                            q.push({ nx, ny });
+                            total += maps[ny][nx] - '0';
+                            maps[ny][nx] = 'X';
+                        }
+                    }
+                }
+
+                answer.push_back(total);
             }
         }
+    }
 
-        answer.push_back(score);
+    if (answer.empty())
+    {
+        answer.push_back(-1);
+    }
+    else
+    {
+        sort(answer.begin(), answer.end());
     }
 
     return answer;
