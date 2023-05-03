aea1cc27d518c01ba12c1f5b3cc1a5ba8800441d
JongWoo <jinlaove17@naver.com>
2023-05-04 01:30:52 +0900

    [Lv.2] 점 찍기

--- 2022_ProgrammersCodingTest/Main.cpp
+++ 2022_ProgrammersCodingTest/Main.cpp
@@ -1,29 +1,16 @@
-#include <vector>
+#include <cmath>
 
 using namespace std;
 
-int solution(int n, int m, vector<int> section)
+long long solution(int k, int d)
 {
-    int answer = 0;
-    vector<int> wall(n + 1, 1);
+    long long answer = 0;
 
-    // 칠해야 할 벽의 부분은 0으로 초기화한다.
-    for (int i = 0; i < section.size(); ++i)
+    for (int x = 0; x <= d; x += k)
     {
-        wall[section[i]] = 0;
-    }
+        long long y = sqrt(static_cast<long long>(d) * d - static_cast<long long>(x) * x);
 
-    for (int i = 1; i <= n; ++i)
-    {
-        // 칠해야 할 벽의 부분이라면,
-        if (wall[i] == 0)
-        {
-            // 롤러의 길이만큼 벽을 오른쪽으로 칠한다.
-            i = i + m - 1;
-
-            // 페인트 칠 횟수를 1 증가시킨다.
-            ++answer;
-        }
+        answer += y / k + 1;
     }
 
     return answer;
